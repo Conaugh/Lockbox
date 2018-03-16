083c86c1da0ad2cc5c3360a6defefb4b7c79910c
Conaugh <conaughparker@yahoo.co.uk>
2018-03-16 18:54:16 +0000

    Add project files.

---
+++ Lockbox/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Lockbox.MvcApplication" Language="C#" %>
---
+++ Lockbox/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lockbox.Startup))]
+namespace Lockbox
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
