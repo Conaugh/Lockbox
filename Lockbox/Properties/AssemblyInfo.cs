759fd53fc0bc144c44b3e8ba40189069e57d752a
Conaugh <conaughparker@yahoo.co.uk>
2018-03-16 20:22:22 +0000

    Update copyright info

--- Lockbox/Properties/AssemblyInfo.cs
+++ Lockbox/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Lockbox")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A dungeon companion")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Conaugh Parker")]
 [assembly: AssemblyProduct("Lockbox")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright © Conaugh Parker 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
